40598b0a8d7023a9583c513a3ed582b33cf2c781
BATU1579 <51915056+batu1579@users.noreply.github.com>
2023-08-31 11:04:31 +0800

    ✨ feat(app.main): 使用读取的设置初始化框架

--- app/main.py
+++ app/main.py
@@ -1,7 +1,42 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
 
-app = FastAPI(
-    title="TRPG APP API",
-    version="0.1.0",
-)
+from app.util.log import Logger
+from app.util.settings import Settings, SettingsManager
 
+
+def create_app() -> FastAPI:
+    """初始化 FastAPI 应用，加载中间件并注册处理器
+
+    Returns:
+        FastAPI: 给 uvicorn 启动服务用的应用对象
+    """
+    settings: Settings = SettingsManager.get_settings()
+
+    app = FastAPI(
+        title="TRPG APP API",
+        version="0.1.0",
+        docs_url=settings.document.docs_path,
+        redoc_url=settings.document.redoc_path,
+    )
+
+    # 注册中间件
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.service.origins,
+        allow_credentials=True,
+        allow_methods=["GET", "POST", "PUT", "DELETE"],
+        allow_headers=["*"],
+    )
+
+    app.add_middleware(
+        GZipMiddleware,
+        minimum_size=settings.service.gzip_min_size,
+    )
+
+    if settings.ssl.redirect:
+        app.add_middleware(HTTPSRedirectMiddleware)
+
+    return app
