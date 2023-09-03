581530fa362d0e905f17437fd2fa69b6aa9b032e
BATU1579 <51915056+batu1579@users.noreply.github.com>
2023-09-03 10:00:59 +0800

    Feature snowflake guid (#4)
    * ✨ feat(app.util.type.guid): 添加雪花算法生成的 guid 类
    
    使用雪花算法生成的 guid 可以在保证全局唯一的同时，让主键无序自增降低被预测的风险
    
    * 🧪 test(test.util.type.test_guid): 添加 guid 的测试用例

--- start.py
+++ start.py
@@ -5,21 +5,40 @@ from pydantic import validate_call
 from typer import Option, Typer
 from uvicorn import run as uvicorn_run
 
-app = Typer(help="TRPG App server")
+from app.util.settings import Settings, SettingsManager
+
+cli = Typer(help="TRPG App server")
 
 
-@app.command()
+@cli.command()
 @validate_call
-def run(env_file: Optional[Path] = Option(None, help="手动指定环境变量文件位置")):
+def run(env_file: Optional[Path] = Option(None, help="手动指定环境变量文件位置")) -> None:
     """启动服务"""
+    if env_file is not None:
+        SettingsManager.set_env_file_path(env_file)
+    SettingsManager.reload_settings()
+
+    settings: Settings = SettingsManager.get_settings()
+
+    keyfile_path: Path | None = settings.ssl.keyfile
+    certfile_path: Path | None = settings.ssl.certfile
+
     uvicorn_run(
-        app="app.main:app",
-        host=SETTINGS.service.host,
-        port=SETTINGS.service.port,
-        ssl_keyfile=keyfile_path,
-        ssl_certfile=certfile_path,
+        app="app.main:create_app",
+        host=settings.service.host,
+        port=settings.service.port,
+        ssl_keyfile=keyfile_path.as_posix() if keyfile_path else None,
+        ssl_certfile=certfile_path.as_posix() if certfile_path else None,
+        factory=True,
     )
 
 
+@cli.command()
+@validate_call
+def test() -> None:
+    """运行服务测试"""
+    print("Start testing...")
+
+
 if __name__ == "__main__":
-    app()
+    cli()
