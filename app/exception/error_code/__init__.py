5283b134856453b2574fb24b38a53073abaadca2
BATU1579 <51915056+batu1579@users.noreply.github.com>
2023-09-08 00:38:54 +0800

    Feature response model (#11)
    * ✨ feat(app.exception.error_code): 添加了异常相关模型
    
    添加了错误码模型 ErrorCodeModel 和他的类型声明，可以方便的在模型和字符串错误码之间转换
    
    * 🦄 refactor(app.exception.error_code): 修改错误码中的模块枚举名称
    
    将 ModuleNumber 和相关的 module_number 字段修改为 ModuleName 和 module_name 使之更语义化
    
    * ✨ feat(app.exception.error_code): 添加模型特定的 repr 方法
    
    提供更有针对性的调试信息
    
    * ✨ feat(app.exception.error_code): 重新导出异常模型相关类
    
    暴露公用的类，展平导入
    
    * ✨ feat(app.exception): 重新导出异常相关类
    
    在最外层暴露常用的类
    
    * ✨ feat(app.model.response): 添加响应模型
    
    添加了 json 响应的模型 JsonResponseModel 和他的两个子类 Success 和 Error
    
    * 📃 docs(CHANGELOG): 更新 changelog

---
+++ app/exception/error_code/__init__.py
@@ -0,0 +1,7 @@
+from app.exception.error_code.error_model import (
+    ErrorCodeModel,
+    ErrorInfo,
+    ErrorType,
+    PydanticErrorCode,
+)
+from app.exception.error_code.module_name import ModuleName
